a0b1f7d2ff35f7ee96dd51d190742c9433184c32
Brain Stone <16119519+Stonerville@users.noreply.github.com>
2018-07-01 07:00:56 -0400

    Updated deliver script: path to jar

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -15,12 +15,13 @@ NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
+echo 'extracts the value of the <artifactId/> element within <project/> instead.'
 set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+ARTIFACTID=`mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
+# VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${ARTIFACTID}-${VERSION}.jar
